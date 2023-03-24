c4936edef29b2dee1c04ee14cb30a462afa51890
AnastasiaZhekina <yfcnzjkzdbnz09@gmail.com>
2023-03-24 08:11:39 +0500

    second

--- Triangle.cpp
+++ Triangle.cpp
@@ -1,17 +1,29 @@
 #include "Triangle.h"
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-Triangle::Triangle() : Figure(3, "Правильная") {
-	a = 10; b = 20; c = 30;
-	A = 50; B = 60; C = 70;
-}
-Triangle::Triangle(int p_a, int p_b, int p_c, int p_A, int p_B, int p_C) {
-	a = p_a; b = p_b; c = p_c;
-	A = p_A; B = p_B; C = p_C;
-}
-void Triangle::show() {
-	cout << "Стороны: " << "a = " << a << " b = " << b << " c = " << c << endl;
-	cout << "Углы: " << "A = " << A << " B = " << B << " C = " << C << endl << endl;
-}
+
+Triangle::Triangle(string p_name, int p_a, int p_b, int p_c, int p_A, int p_B, int p_C) {
+		a = p_a; b = p_b; c = p_c;
+		A = p_A; B = p_B; C = p_C;
+		name = p_name;
+	}
+	string Triangle::gets()  { return name; }
+	int Triangle::getxa() { return a; }
+	int Triangle::getxb() { return b; }
+	int Triangle::getxc() { return c; }
+	int Triangle::getxA(){ return A; }
+	int Triangle::getxB() { return B; }
+	int Triangle::getxC() { return C; }
+
+	Triangle::Triangle() {
+		a = 10; b = 20; c = 30;
+		A = 50; B = 60; C = 70;
+		name = "Треугольник";
+	}
+
+	void Triangle::print_info() {
+		cout << gets() << endl << "Стороны: " << "a = " << getxa() << " b = " << getxb() << " c = " << getxc() << endl;
+		cout << "Углы: " << "A = " << getxA() << " B = " << getxB() << " C = " << getxC() << endl << endl;
+	}
--- Triangle.h
+++ Triangle.h
@@ -1,11 +1,24 @@
 #pragma once
+#include <string>
 #include "Figure.h"
-class Triangle : public Figure {
-	int a, b, c;
-	int A, B, C;
-public:
-	Triangle();
-	Triangle(int p_a, int p_b, int p_c, int p_A, int p_B, int p_C);
-	void show();
-};
 
+class Triangle : public Figure {
+protected:
+	int a, b, c;
+	int A, B , C;
+	string name ;
+public:
+
+	Triangle(string p_name, int p_a, int p_b, int p_c, int p_A, int p_B, int p_C);
+	string gets() override;
+	int getxa() override;
+	int getxb();
+	int getxc();
+	int getxA()override;
+	int getxB();
+	int getxC();
+
+	Triangle();
+
+	void print_info() override;
+};
